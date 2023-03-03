723eb92ea59397755e11efc90637b19d99b31ef8
Milena <milenagg77@hotmail.com>
2023-03-03 17:29:57 +0000

    coorected code adding function to Write Code to gather information about the manager team member

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
 
+const teamMembers = [];
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
 //const inquirer = require("inquirer");
 //const Manager = require("./lib/Manager");
+
+
+function addManager() {
 
 inquirer
     .prompt([
@@ -46,8 +50,11 @@ inquirer
             answers.email,
             answers.officeNumber
         );
-        console.log(manager);
+        //console.log(manager);
+        teamMembers.push(manager);
+        presentMenu();
     });
+}
 
 // const inquirer = require("inquirer");
 // const Engineer = require("./lib/Engineer");
